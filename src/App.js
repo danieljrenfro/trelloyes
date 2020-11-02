bd2f327cabcbd01385e45c898e4be6d10698db4a
danieljrenfro <djrenfro95@gmail.com>
2020-11-02 15:44:20 -0600

    add App, List & Card to Trelloyes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import List from './List';
 import './App.css';
 
-function App() {
+function App(props) {
+  
+  let allCards = props.store.STORE.allCards;
+
+  function cardsArray(list) {
+    let cards = list.cardIds.map(cardId => {
+      return allCards[cardId];
+    });
+    return cards;
+  }
+  
+  let cardLists = props.store.STORE.lists.map(list => {
+    return <List key={list.id} cards={cardsArray(list)} header={list.header} />;
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className='App'>
+      <header className='App-header'>
+        <h1>Trelloyes</h1>
       </header>
-    </div>
+      <div className='App-list'>
+        {cardLists}
+      </div>
+    </main>
   );
+    
 }
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from './Card';
+import './List.css';
+
+
+
+function List(props) {
+
+  let cards = props.cards.map(card => {
+    return <Card key={card.id} title={card.title} content={card.content} />
+  })
+
+    return (
+      <section className='List'>
+        <header className='List-header'>
+          <h2>{props.header}</h2>
+        </header>
+        <div className='List-cards'>
+          {cards}
+        </div>
+      </section>
+    );
+}
+
+export default List;
